e3b7970147eb879314dd5276c7f4976e34fda133
danBamikiya <dudeawesome732@gmail.com>
2021-11-21 14:56:58 +0100

    feat: add Directory component :lipstick:

---
+++ src/components/Directory/index.tsx
@@ -0,0 +1,30 @@
+import ShopMen from '../../assets/shopMens.jpg'
+import ShopWomen from '../../assets/shopWomens.jpg'
+import './styles.scss'
+
+const Directory = (props: any) => {
+  return (
+    <div className="directory">
+      <div className="wrap">
+        <div
+          className="item"
+          style={{
+            backgroundImage: `url(${ShopWomen})`
+          }}
+        >
+          <a>Shop Womens</a>
+        </div>
+        <div
+          className="item"
+          style={{
+            backgroundImage: `url(${ShopMen})`
+          }}
+        >
+          <a>Shop Mens</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Directory

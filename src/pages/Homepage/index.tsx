87d219f4bde5cff7d4f15a8f22ad4711b3b19c5d
danBamikiya <dudeawesome732@gmail.com>
2021-11-21 14:55:36 +0100

    feat: add Homepage component :lipstick:

---
+++ src/pages/Homepage/index.tsx
@@ -0,0 +1,13 @@
+import './styles.scss'
+import Directory from '../../components/Directory'
+
+const Homepage = (props: any) => {
+  return (
+    <section className="homepage">
+      <Directory />
+      <h1>Homepage</h1>
+    </section>
+  )
+}
+
+export default Homepage

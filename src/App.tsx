ca2d769a3f8d98aa6c9642287c449bcdf7a3586c
danBamikiya <dudeawesome732@gmail.com>
2021-12-18 00:29:48 +0100

    feat: handle routing and authentication :sparkles:

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,101 @@
-import React from 'react'
+import { Component } from 'react'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import './default.scss'
-import Header from './components/Header'
-import Homepage from './pages/Homepage'
-// import logo from './logo.svg';
+import { auth, handleUserProfile } from './firebase/utils'
+import { UserState } from './types/user'
+import { Unsubscribe } from 'firebase'
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <div className="main">
-        <Homepage />
+// layouts
+import HomepageLayout from './layouts/HomepageLayout'
+import MainLayout from './layouts/MainLayout'
+
+// pages
+import Homepage from './pages/Homepage'
+import Registration from './pages/Registration'
+import Login from './pages/Login'
+
+interface Props {}
+interface State extends UserState {}
+
+const intialState = {
+  currentUser: null
+}
+
+class App extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      ...intialState
+    }
+  }
+
+  authListener: Unsubscribe | null = null
+
+  componentDidMount() {
+    this.authListener = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await handleUserProfile(userAuth)
+        userRef?.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          })
+        })
+      }
+
+      this.setState({
+        ...intialState
+      })
+    })
+  }
+
+  componentWillUnmount() {
+    this.authListener!()
+  }
+
+  render() {
+    const { currentUser } = this.state
+    return (
+      <div className="App">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <HomepageLayout currentUser={currentUser}>
+                <Homepage />
+              </HomepageLayout>
+            }
+          />
+          <Route
+            path="/registration"
+            element={
+              currentUser ? (
+                <Navigate to="/" />
+              ) : (
+                <MainLayout currentUser={currentUser}>
+                  <Registration />
+                </MainLayout>
+              )
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              currentUser ? (
+                <Navigate to="/" />
+              ) : (
+                <MainLayout currentUser={this.state.currentUser}>
+                  <Login />
+                </MainLayout>
+              )
+            }
+          />
+        </Routes>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App

13b9ab337baa6eb3927e3b1ea0b5cdc47fba7151
danBamikiya <dudeawesome732@gmail.com>
2021-12-18 00:09:51 +0100

    feat: toggle Header CTAs when user is authenticated :passport_control:

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,45 @@
-import './styles.scss'
+import { FC } from 'react'
+import { Link } from 'react-router-dom'
+import { UserState } from '../../types/user'
+import { auth } from '../../firebase/utils'
 import Logo from '../../assets/logo.png'
+import './styles.scss'
 
-const Header = (props: any) => {
+type props = UserState
+
+const Header: FC<props> = ({ currentUser }) => {
+  const handleSignOut = () => {
+    auth.signOut()
+  }
+
   return (
     <header className="header">
       <div className="wrap">
         <div className="logo">
-          <img src={Logo} alt="Logo" />
+          <Link to="/">
+            <img src={Logo} alt="Logo" />
+          </Link>
+        </div>
+
+        <div className="callToActions">
+          {currentUser && (
+            <ul>
+              <li>
+                <span onClick={handleSignOut}>LogOut</span>
+              </li>
+            </ul>
+          )}
+
+          {!currentUser && (
+            <ul>
+              <li>
+                <Link to="/registration">Register</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          )}
         </div>
       </div>
     </header>

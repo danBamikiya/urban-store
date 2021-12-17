030c5f95f5fac8d7590ce5d0a539e62005311d71
danBamikiya <dudeawesome732@gmail.com>
2021-12-18 00:05:17 +0100

    feat: utilities for interacting with firebase :passport_control:

---
+++ src/firebase/utils.ts
@@ -0,0 +1,44 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+import { firebaseConfig } from './config'
+import { user } from '../types/user'
+
+firebase.initializeApp(firebaseConfig)
+
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
+
+export const GoogleProvider = new firebase.auth.GoogleAuthProvider()
+GoogleProvider.setCustomParameters({ prompt: 'select_account' })
+
+export const signInWithGoogle = () => auth.signInWithPopup(GoogleProvider)
+
+export const handleUserProfile = async (
+  userAuth: user,
+  additionalData?: any
+) => {
+  if (!userAuth) return
+  const { uid } = userAuth
+
+  const userRef = firestore.doc(`users/${uid}`)
+  const snapshot = await userRef.get()
+
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth
+    const timestamp = new Date()
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdDate: timestamp,
+        ...additionalData
+      })
+    } catch (err) {
+      // console.log(err)
+    }
+  }
+
+  return userRef
+}

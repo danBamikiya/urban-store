ea5fefb6aa0b68641571de62b56a80ad82227e6d
danBamikiya <dudeawesome732@gmail.com>
2021-12-18 00:13:00 +0100

    feat: add Footer component :lipstick:

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import './styles.scss'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="wrap">@ Urban Stores 2021</div>
+    </footer>
+  )
+}
+
+export default Footer

c4e7b4fa6895ad7d9101f572258186c5764d0bd7
danBamikiya <dudeawesome732@gmail.com>
2022-04-09 17:10:25 +0100

    fix footer, and product card styling

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,37 @@
+import { Link } from 'react-router-dom'
+import Logo from '../../assets/logo.png'
 import './styles.scss'
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="wrap">@ Urban Stores 2021</div>
+      <div className="wrap">
+        <div className="logo">
+          <Link to="/">
+            <img src={Logo} alt="Logo" />
+          </Link>
+        </div>
+        <div className="links">
+          <div>
+            <Link to="/search">Shop</Link>
+            <Link to="#">Map</Link>
+            <Link to="#">Legal</Link>
+          </div>
+          <div>
+            <Link to="#">About</Link>
+            <Link to="#">Privacy Policy</Link>
+            <Link to="#">Terms & Condition</Link>
+          </div>
+          <div>
+            <span>Social Media</span>
+            <div className="social-icons">
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </footer>
   )
 }
